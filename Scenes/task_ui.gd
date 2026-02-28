e4bc1316198fde1ed1aa2a24b961e48dcdf5fc42
Miky8745 <mikulas.vanecek@gmail.com>
2026-02-28 23:55:20 +0100

    Fixed and added tasks (` to toggle)

--- Scenes/task_ui.gd
+++ Scenes/task_ui.gd
@@ -5,10 +5,32 @@ extends Control
 @onready var label_description: Label = $VBoxContainer/LabelDescription
 @onready var label_name: Label = $VBoxContainer/LabelName
 @onready var label_progress: Label = $VBoxContainer/LabelProgress
+@onready var check_button: CheckButton = $VBoxContainer/CheckButton
+@onready var panel: Panel = $Panel
+@onready var v_box_container: VBoxContainer = $VBoxContainer
+
+var current_async_task = TaskManager.get_completed_tasks_count()
+
 
 func _process(_delta):
+	_sync_panel()
 	update_ui()
-
+	
+	if current_async_task != TaskManager.get_completed_tasks_count():
+		check_button.set_pressed_no_signal(false)
+		current_async_task = TaskManager.get_completed_tasks_count()
+	
+	if check_button.button_pressed:
+		print("button pressed")
+		print(TaskManager.get_current_task().required_item_id)
+		if Utils.remove_resources_safe([{"id": TaskManager.get_current_task().required_item_id, "amount": 1}]):
+			print("resource going")
+			TaskManager.current_task_progress += 1
+	
+	if TaskManager.current_task_progress >= TaskManager.get_current_task().required_amount:
+		TaskManager.complete_current_task()
+		
+		
 func update_ui():
 	var manager = TaskManager
 	var task = manager.get_current_task()
@@ -39,3 +61,7 @@ func update_ui():
 	]
 	progress_bar.value = TaskManager.current_task_progress
 	progress_bar.max_value = TaskManager.get_current_task().required_amount
+	
+func _sync_panel():
+	panel.position = v_box_container.position
+	panel.scale = v_box_container.scale
