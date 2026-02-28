7f15132cf3e6e8de7ee32b672bcb5dff6ee56336
TomasKruta <tomas.kruta@outlook.cz>
2026-02-28 11:34:15 +0100

    add Task

---
+++ Scripts/Task.gd
@@ -0,0 +1,10 @@
+class_name Task
+
+var id: String
+var type: int
+var name: String
+var description: String
+
+var required_item_id: String
+var required_machine_type: int
+var required_amount: int
