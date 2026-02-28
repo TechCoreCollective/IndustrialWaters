58f441aaffe2947e006fce252773b073d4f69d8d
hobitek-51 <kielbus2@mikulasske.cz>
2026-02-28 21:42:28 +0100

    merge

--- Scenes/soundtrack.gd
+++ Scenes/soundtrack.gd
@@ -1,14 +1,12 @@
-extends Node2D  # Nebo Node, podle toho co je hlavní uzel
+extends Node2D  
 
-# Pomocí $ získáme přístup k přehrávači hudby
 @onready var audio_player = $AudioStreamPlayer
 
 func _ready() -> void:
 	unmute_audio()
 
 func _on_mutebutton_pressed():
-	# Tato funkce se spustí, když klikneš na tlačítko
-	# (Předpokládám, že jsi signál 'pressed' připojil sem do skriptu)
+	
 	
 	if audio_player.playing:
 		mute_audio()
